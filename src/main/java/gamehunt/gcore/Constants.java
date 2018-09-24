3a83f7ed05fa440c3964ddd646deb35a39a1fd87
gamehunt <mt.demakov@gmail.com>
2018-09-24 15:43:31 +0300

    Updated build system

--- src/main/java/gamehunt/gcore/Constants.java
+++ src/main/java/gamehunt/gcore/Constants.java
@@ -3,7 +3,8 @@ package gamehunt.gcore;
 public class Constants {
 	public static final String MODID = "gcore";
 	public static final String NAME = "GCore";
-	public static final String VERSION = "0.13.2";
+	public static final String BUILD = "GRADLE:BUILD";
+	public static final String VERSION = "GRADLE:VERSION-" + BUILD;
 	public static final String COMMON_PROXY = "gamehunt.gcore.CommonProxy";
 	public static final String CLIENT_PROXY = "gamehunt.gcore.ClientProxy";
 
