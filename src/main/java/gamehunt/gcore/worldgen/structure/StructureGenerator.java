c2ab71e4ed95f911c4b07c6db4c2f66965e3e680
gamehunt <mt.demakov@gmail.com>
2019-01-22 14:40:32 +0300

    Fixed structure generator

--- src/main/java/gamehunt/gcore/worldgen/structure/StructureGenerator.java
+++ src/main/java/gamehunt/gcore/worldgen/structure/StructureGenerator.java
@@ -6,6 +6,7 @@ import java.util.Random;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.structure.template.PlacementSettings;
@@ -27,7 +28,11 @@ public class StructureGenerator implements IWorldGenerator{
 			IChunkProvider chunkProvider) {
 		// TODO Auto-generated method stub
 			for(IStructure s : struct_list){
-				BlockPos basePos = new BlockPos(chunkX * 16 + random.nextInt(16), 100, chunkZ * 16 + random.nextInt(16));
+				int x = (chunkX << 4) + random.nextInt(8);
+			    int z = (chunkZ << 4) + random.nextInt(8);
+				int y = random.nextInt(world.getActualHeight());
+			    BlockPos basePos = new BlockPos(x, y, z);
+				
 				if(s.canGenerateHere(world, basePos)){
 					final PlacementSettings settings = new PlacementSettings().setRotation(Rotation.NONE);
 		            final Template template = world.getSaveHandler().getStructureTemplateManager().getTemplate(world.getMinecraftServer(), s.getStructure());
